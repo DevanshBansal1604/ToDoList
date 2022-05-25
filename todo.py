605d99cdbfbf3396068f44f11cd1cd1c792c6ddb
Devansh Bansal <75175162+Learner-1604@users.noreply.github.com>
2022-05-25 23:16:01 +0530

    commit

--- todo.py
+++ todo.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +11,34 @@ def func():
 @app.route("/sub", methods=['POST'])
 def submit():
     if request.method=="POST":
-        return render_template("sub.html")
+
+        desc=request.form['desc']
+        dead=request.form['dead']
+
+        pres=str(datetime.now().time())
+        pres=datetime.strptime(pres,"%H:%M:%S.%f")
+
+        print(pres)
+
+        dead=datetime.strptime(dead, "%H:%M")
+
+        diff=dead-pres
+
+        ls.append((desc,diff))
+
+        for i in range(1, len(ls)):
+            j = i-1
+            while j >= 0:
+                if ls[i][1] < ls[j][1]:
+                    ls[i], ls[j] = ls[j], ls[i]
+                
+                j-=1
+        
+        print(ls)
+        msg="Task Has Been Added Successfully!"
+        
+    return render_template("sub.html",todolist=ls,msgs=msg)
 
 if __name__ == "__main__":
+    ls=[]
     app.run(debug=True)
