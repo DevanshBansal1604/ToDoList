b4a36070598afe5c6029b6346848dddda8ad10ee
cookie-18 <86962799+cookie-18@users.noreply.github.com>
2022-05-27 17:27:51 +0530

    commit

--- todo.py
+++ todo.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def func():
     msg="empty"
@@ -13,37 +14,73 @@ def func():
 @app.route("/",methods=["POST"])
 def submit():
     if request.method=="POST":
-
-        desc=request.form['desc']
-        dead=request.form['dead']
-
-        pres=str(datetime.now().time())
-        pres=datetime.strptime(pres,"%H:%M:%S.%f")
-
-        print(pres)
-
-        strdead=dead
-
-        dead=datetime.strptime(dead, "%H:%M")
-
-        diff=dead-pres
-
-        ls.append((desc,diff.seconds,strdead))
-
-
-        for i in range(1, len(ls)):
-            k=i
-            j = k-1
-            while j >= 0:
-                if ls[k][1] < ls[j][1]:
-                    ls[k], ls[j] = ls[j], ls[k]
-                
-                k-=1
-                j-=1
         
-        print(ls)
-        msg="Task Has Been Added Successfully!"
-        return render_template("index.html",todolist=ls,msgs=msg)
+        if 'desc' in request.form.keys():
+            desc=request.form['desc']
+            dead=request.form['dead']
+
+            pres=str(datetime.now().time())
+            pres=datetime.strptime(pres,"%H:%M:%S.%f")
+
+            print(pres)
+
+            strdead=dead
+
+            dead=datetime.strptime(dead, "%H:%M")
+
+            diff=dead-pres
+
+            ls.append((desc,diff.seconds,strdead))
+
+
+            for i in range(1, len(ls)):
+                k=i
+                j = k-1
+                while j >= 0:
+                    if ls[k][1] < ls[j][1]:
+                        ls[k], ls[j] = ls[j], ls[k]
+                    
+                    k-=1
+                    j-=1
+            
+            print(ls)
+            msg="Task Has Been Added Successfully!"
+            return render_template("index.html",todolist=ls,msgs=msg)
+
+
+        else:
+            print("Task",request.form['Done'])
+            msg="Task Has Been Deleted Successfully!"
+            return render_template("index.html",todolist=ls,msgs=msg)
+            
+
+        # pres=str(datetime.now().time())
+        # pres=datetime.strptime(pres,"%H:%M:%S.%f")
+
+        # print(pres)
+
+        # strdead=dead
+
+        # dead=datetime.strptime(dead, "%H:%M")
+
+        # diff=dead-pres
+
+        # ls.append((desc,diff.seconds,strdead))
+
+
+        # for i in range(1, len(ls)):
+        #     k=i
+        #     j = k-1
+        #     while j >= 0:
+        #         if ls[k][1] < ls[j][1]:
+        #             ls[k], ls[j] = ls[j], ls[k]
+                
+        #         k-=1
+        #         j-=1
+        
+        # print(ls)
+        # msg="Task Has Been Added Successfully!"
+        # return render_template("index.html",todolist=ls,msgs=msg)
 
 if __name__ == "__main__":
     ls=[]
